56c53c08757d01bfa17b3a633c2f783fc6152af2
Luferov Victor <lyferov@yandex.ru>
2023-02-26 17:30:53 +0300

    fix: typing and background

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -2,16 +2,16 @@
 <section class="lg:h-screen md:h-screen flex-wrap py-20 md:py-0">
   <div class="lg:h-screen md:h-screen flex flex-row justify-space-between flex-wrap">
     <div class="basis-1/2 prose max-w-prose ma-auto text-neutral-900 pa-5 md:pa-10">
-      <div class="text-5xl text-lime-300 uppercase">Меня зовут</div>
-      <div class="text-5xl text-lime-300 uppercase whitespace-nowrap">Алиса Быкова</div>
-      <p class="text-3xl text-lime-300 uppercase my-10">
+      <div class="text-5xl md:text-3xl lg:text-5xl text-lime-300 uppercase">Меня зовут</div>
+      <div class="text-5xl md:text-3xl lg:text-5xl text-lime-300 uppercase whitespace-nowrap">Алиса Быкова</div>
+      <p class="text-3xl md:text-2xl lg:text-3xl text-lime-300 uppercase my-10">
         Я нумеролог
       </p>
       <p class="text-2xl text-lime-300 uppercase">
-        работаю по методу статистического психоанализ
+        работаю по методу статистического психоанализа
       </p>
     </div>
-    <div class="hidden md:block basis-1/2 bg-[url('/Alice.jpg')] bg-cover"></div>
+    <div class="hidden md:block basis-1/2 bg-[url('/Alice.jpg')] bg-cover bg-center"></div>
   </div>
 </section>
 </template>
